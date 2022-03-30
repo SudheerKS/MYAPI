dcd7852d4932a6ac633cb291a2f1909230e5492e
SudheerKS <sudheer9.net@gmail.com>
2022-03-30 21:38:19 +0100

    first commit

---
+++ PricingTool.API/Controllers/AppController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PricingTool.API.Controllers
+{
+    [Route("App/[action]")]
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        public string Get()
+        {
+            return "test";
+        }
+    }
+}
